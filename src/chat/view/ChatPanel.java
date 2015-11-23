78a6a8e32f4179d1d2bac0d9e6be5be6122df1c8
Tyler jarrard <tjar2074@740s-pl-104.local>
2015-11-23 09:33:23 -0700

    chat frame set up

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,5 +1,26 @@
 package chat.view;
 
-public class ChatPanel {
+import javax.swing.*;
+import chat.controller.ChatController;
+import java.awt.Color;
+import java.awt.event.*;
 
+public class ChatPanel extends JPanel
+{
+	private ChatController baseController;
+	private JButton firstButton;
+	private JTextField firstTextField;
+	private SpringLayout baseLayout;
+	
+	public ChatPanel(ChatController baseController)
+	{
+		this.baseController = baseController;
+		
+		baseLayout = new SpringLayout();
+//		firstButton = new JButton("Please do not click the button");
+//		firstTextField = new JTextField("words can be type here");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 }
